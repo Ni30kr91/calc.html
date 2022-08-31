26e64d6b85cfac662d3158cba823e1b753d44b94
Ni30kr91 <nitish.kumar8092@gmail.com>
2022-08-31 16:09:39 +0530

    done

---
+++ script.js
@@ -0,0 +1,18 @@
+function display(a){
+console.log(a)
+    b=document.getElementById("display").value;
+    if(a=="c"){
+        document.getElementById("display").value="";
+       }else{
+        console.log(b)
+     x=b+a
+     document.getElementById("display").value=x
+    }
+   
+}
+ 
+ function calculate(){
+     b=document.getElementById("display").value;
+     console.log(eval(b))
+     document.getElementById("display").value=eval(b)
+ }
